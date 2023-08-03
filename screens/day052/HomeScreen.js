94decff808efc8df3274c3db73aa3780d0f84a69
maazjnr <goalmaaz@gmail.com>
2023-08-03 14:43:06 +0100

    update

--- screens/day052/HomeScreen.js
+++ screens/day052/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { StyleSheet, Pressable } from 'react-native';
+import { StyleSheet, Pressable, View, TouchableOpacity } from 'react-native';
 import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 import LottieView from 'lottie-react-native';
 
@@ -19,37 +19,37 @@ const HomeScreen = ({ navigation }) => {
     return (
 
         <Pressable style={styles.animationContainer} onPress={() => { opacity.value = 0; animation.current.play() }}>
-            <LottieView
-                autoPlay={true}
-                onAnimationFinish={() => {
-                    opacity.value = withTiming(1, { duration: 1000 })
-                }}
-                loop={false}
-                ref={animation}
-                style={{
-                    width: 400,
-                    height: 400,
-                    backgroundColor: 'rgba(0,0,0,0)',
-                }}
-                source={require('./45722-rocket-loader.json')}
-            />
-            <Animated.Text style={[{ position: 'absolute', fontSize: 35, fontWeight: '800', color: '#2c3e50' }, rStyle]}>React Native Space</Animated.Text>
-        </Pressable>
+        <LottieView
+            autoPlay={true}
+            onAnimationFinish={() => {
+                opacity.value = withTiming(1, { duration: 1000 })
+            }}
+            loop={false}
+            ref={animation}
+            style={{
+                width: 400,
+                height: 400,
+                backgroundColor: 'rgba(0,0,0,0)',
+            }}
+            source={require('./45722-rocket-loader.json')}
+        />
+        <Animated.Text style={[{ position: 'absolute', fontSize: 35, fontWeight: '800', color: '#2c3e50' }, rStyle]}>Maazscript</Animated.Text>
+    </Pressable>
 
     );
 }
 
 const styles = StyleSheet.create({
     animationContainer: {
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flex: 1,
+        backgroundColor: "#FFF",
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
     },
     buttonContainer: {
-      paddingTop: 20,
+        paddingTop: 21
     },
-  });
-  
+});
+
 
 export { HomeScreen };
